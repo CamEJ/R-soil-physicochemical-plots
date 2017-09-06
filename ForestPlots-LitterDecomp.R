34ddfc4bd1a40d5e325d35f7ea5d7fec0b4f2603
Cam <camillathorn85@gmail.com>
2017-09-06 16:44:18 +0100

    Update ForestPlots-LitterDecomp.R

--- ForestPlots-LitterDecomp.R
+++ ForestPlots-LitterDecomp.R
@@ -17,35 +17,52 @@
 # the excel doc from which this data is taken is in litter decomp folder
 # and a sheet in doc called 'LitterDeg-compliled
 
-setwd("C:/Users/Camilla/Dropbox/Data & analysis/WP3 Slurry disturbance/R")
+setwd("C:/Users/Camilla/Dropbox/Data & analysis/WP3 Slurry disturbance/Plotting Data/PhenotypicData-R")
 library(ggplot2)
 
-D1 <- read.table(file="LitterDecompForest.txt", header=T, sep='\t')
+D1 <- read.table(file="LitterDecompAll-Tps.txt", header=T, sep='\t')
 
-D1$timepoint <- factor(D1$timepoint, levels = D1$timepoint)
-# do this so it keeps time points in order
-# don't worry too much about resulting warning message
-D1$Treatment <- factor(D1$Treatment, levels = D1$Treatment)
-# and again so it keeps treatments in correct order in legend. 
+# 3. run setFactorOrder function (R basics folder)
+# then do the following to ensure in correct order when plotting
+
+# order you want timepoints to occur
+D1[["timepoint"]] <- setFactorOrder(D1[["timepoint"]], c("T0", "T1", "T2", "T3", "T4", "T5","T6","T7", "T8", "T9","T10","T11","T12", "T13"))
+
+# order of treatments. 
+D1[["Treatment"]] <- setFactorOrder(D1[["Treatment"]], c("Control", "Slurry", "Flood", "Flood+Slurry"))
+
 
 p <- ggplot(D1, aes(x=timepoint, y=LitterDecomp, ymin=LitterDecomp-pSd, ymax=LitterDecomp+pSd))+
   geom_pointrange(size=0.75)+
   geom_hline(yintercept = 1.72, linetype=2)+ # y intercept= Av of all control resp
   coord_flip(ylim = c(0.12, 7))+
   xlab('Time Point')+ 
-  ylab(expression(paste('Litter decomposition rate (', mu, 'g ', C-CO[2],' ', g^-1, 'soil ', hr^-1, ' )' ))) +
+  ylab(expression(paste('Litter decomposition rate (', mu, 'g ', C-CO[2],' ', g^-1, 'soil ', hr^-1, ')' ))) +
   geom_point(colour="black", shape=21, size = 10) +
   aes(fill = factor(Treatment)) + 
   scale_fill_manual(values=c("black", "chocolate4", "slateblue", "olivedrab"))
 p
 
+# legend on the top
 p2 <- p + theme_bw() + 
-  theme(axis.text.y=element_text(size=17, colour="black")) +
-  theme(axis.text.x=element_text(size=16, colour="black"))+
-  #theme(legend.title=element_text(name="Treatment"))
-  theme(legend.text = element_text(size=15)) +
-  theme(legend.title = element_text(size=16)) +
-  labs(fill="Treatment") 
+  theme(axis.text.y=element_text(size=16, colour="black"),
+        axis.text.x=element_text(size=16, colour="black"),
+        axis.title.x=element_text(size=16),
+        legend.text = element_text(size=16),
+        legend.title = element_blank(),
+        axis.title.y=element_blank())+
+  theme(legend.position="top") 
 p2
 
+
+# legend on side with heading
+p2 <- p + theme_bw() + 
+  theme(axis.text.y=element_text(size=16, colour="black"),
+        axis.text.x=element_text(size=16, colour="black"),
+        axis.title.x=element_text(size=16),
+        legend.text = element_text(size=16),
+        legend.title = element_text(size=17),
+        axis.title.y=element_blank())+
+  labs(fill="Treatment") 
+p2
 
